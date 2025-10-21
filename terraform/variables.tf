017739ffb631f85597c7fd1d7f993b89f9503cd3
Milan <milan.jugovic_bb@outlook.com>
2025-10-21 14:29:04 +0200

    Format Terraform variables file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -37,8 +37,8 @@ variable "common_tags" {
   description = "Common tags to be applied to all resources"
   type        = map(string)
   default = {
-    Project     = "Nautilus"
-    CostCenter  = "Engineering"
-    Owner       = "DevOps-Team"
+    Project    = "Nautilus"
+    CostCenter = "Engineering"
+    Owner      = "DevOps-Team"
   }
 }
